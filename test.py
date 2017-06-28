d704267ce58b3f493e3d280bc3b14e8d3476b24e
suzuke <suzuke@hotmail.com>
2017-06-29 00:34:28 +0800

    調整layout

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ class CustomWidget(tk.Frame):
         self.filename = tk.StringVar()
 
         self.root = ttk.LabelFrame(self, text=n)
-        self.root.grid(padx=30, pady=5, ipadx=1, ipady=1)
+        self.root.grid(padx=1, pady=5, ipadx=1, ipady=1, sticky="EW")
 
         self.filenameLabel = ttk.Label(self.root, text="檔案名稱")
         self.filenameLabel.grid(ipadx=10, padx=10, pady=0, row=0, column=0)
@@ -21,7 +21,7 @@ class CustomWidget(tk.Frame):
         self.pathLabel = ttk.Label(self.root, text="檔案歸檔路徑")
         self.pathLabel.grid(ipadx=10, padx=10, pady=0, row=0, column=1)
 
-        self.pathEntry = ttk.Entry(self.root, textvariable=self.path)
+        self.pathEntry = ttk.Entry(self.root, textvariable=self.path, width=25)
         self.pathEntry.grid(ipadx=10, padx=1, pady=0, row=1, column=1)
 
         self.pathButton = ttk.Button(self.root, text="選擇資料夾", command=self.select_path)
@@ -55,39 +55,48 @@ class Application(ttk.Frame):
         self.applicationRoot.grid(row=0, column=0, padx=30, pady=5)
 
         # first
-        self.first = ttk.LabelFrame(self.applicationRoot, text="")
-        self.first.grid(row=0, column=0, padx=15, pady=5, ipadx=171, ipady=1)
+        self.first = ttk.LabelFrame(self.applicationRoot, text="A.選擇要歸類處理的資料夾")
+        self.first.grid(row=0, column=0, padx=1, pady=5, ipadx=171, ipady=1)
         self.first.grid_columnconfigure(0, weight=1)
         self.first.grid_rowconfigure(0, weight=1)
 
-        self.workingDirectoryLabel = ttk.Label(self.first, text="選擇要歸類處理的資料夾:")
-        self.workingDirectoryLabel.grid(row=0, column=0, sticky="EW")
-
         self.workingDirectoryPath = tk.StringVar()
         self.workingDirectoryPathEntry = ttk.Entry(self.first, textvariable=self.workingDirectoryPath)
-        self.workingDirectoryPathEntry.grid(row=1, column=0, sticky="EW")
+        self.workingDirectoryPathEntry.grid(row=0, column=0, sticky="EW")
 
         self.workingDirectoryPathButton = ttk.Button(self.first, text="選擇資料夾", command=self.select_working_path)
-        self.workingDirectoryPathButton.grid(row=1, column=1, sticky="EW")
+        self.workingDirectoryPathButton.grid(row=0, column=1, sticky="EW")
 
         self.startButton = ttk.Button(self.first, text="一鍵歸類", command=self.start)
-        self.startButton.grid(row=2, column=0, sticky="EWNS", columnspan=2)
+        self.startButton.grid(row=1, column=0, sticky="EWNS", columnspan=2)
 
         # second
-        self.second = ttk.LabelFrame(self.applicationRoot, text="")
-        self.second.grid(row=1, column=0, padx=15, pady=5, ipadx=230, ipady=1)
+        self.second = ttk.LabelFrame(self.applicationRoot, text="B.歸類檔案項目設定")
+        self.second.grid(row=1, column=0, padx=1, pady=5, ipadx=230, ipady=1)
         self.second.grid_columnconfigure(0, weight=1)
         self.second.grid_rowconfigure(0, weight=1)
 
-        self.addSettingLabel = ttk.Label(self.second, text="歸類檔案項目設定")
-        self.addSettingLabel.grid(row=0, column=0, sticky="EW")
-
         self.addSettingButton = ttk.Button(self.second, text="+新増歸類檔案項目", command=self.add)
-        self.addSettingButton.grid(row=1, column=0, sticky="EW")
+        self.addSettingButton.grid(row=0, column=0, sticky="EW")
 
         # third
-        self.third = ttk.LabelFrame(self.applicationRoot, text="")
-        self.third.grid(row=2, column=0, padx=30, pady=5, ipadx=1, ipady=1)
+        self.third = ttk.LabelFrame(self.applicationRoot, text="C.歸類檔案項目")
+        self.third.grid(row=2, column=0, padx=1, pady=5, ipadx=1, ipady=1, sticky="EW")
+        self.third.grid_columnconfigure(0, weight=1)
+        self.third.grid_rowconfigure(0, weight=1)
+
+        self.canvas = tk.Canvas(self.third, borderwidth=0)
+        self.canvas.grid(row=0, column=0, sticky="NSWE")
+
+        self.frame = tk.Frame(self.canvas)
+
+        self.vsb = ttk.Scrollbar(self.third, orient="vertical", command=self.canvas.yview)
+        self.vsb.grid(row=0, column=1, sticky="NS")
+        self.canvas.configure(yscrollcommand=self.vsb.set)
+
+        self.canvas.create_window((4, 4), window=self.frame, anchor="nw", tags="self.frame")
+
+        self.frame.bind("<Configure>", self.OnFrameConfigure)
 
         self.customWidgetList = []
 
@@ -101,7 +110,7 @@ class Application(ttk.Frame):
     def add(self):
         i = len(self.customWidgetList) + 1
         print("add %d" % i)
-        widget = CustomWidget(self.third, str(i), self.remove)
+        widget = CustomWidget(self.frame, str(i), self.remove)
         self.customWidgetList.append(widget)
         widget.grid(row=i, column=0, sticky="EW")
         self.grid_rowconfigure(i - 1, weight=1)
@@ -113,8 +122,13 @@ class Application(ttk.Frame):
             self.customWidgetList[i].set_label_n(i+1)
             self.customWidgetList[i].grid(row=(i+1), column=0, sticky="EW")
 
+    def OnFrameConfigure(self, event):
+        '''Reset the scroll region to encompass the inner frame'''
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+
 if __name__ == "__main__":
     root = tk.Tk()
+    root.resizable(False, False)
     root.title("檔案自動分類")
     app = Application(root)
     app.grid(row=0, column=0)
