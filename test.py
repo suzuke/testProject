b834f5a6f4bf01d28b71b5ad21a9007307f6a5f4
suzuke <suzuke@hotmail.com>
2017-07-02 15:25:24 +0800

    增加scrollableContainer

--- test.py
+++ test.py
@@ -8,9 +8,9 @@ from tkinter import messagebox
 import json
 import os
 
-class CustomWidget(tk.Frame):
-    def __init__(self, parent, n, remove_callback, data=""):
-        tk.Frame.__init__(self, parent)
+class CustomWidget(ttk.Frame):
+    def __init__(self, parent, n, remove_callback, data="", **kwargs):
+        ttk.Frame.__init__(self, parent, **kwargs)
 
         self.path = tk.StringVar()
         self.filename = tk.StringVar()
@@ -19,21 +19,28 @@ class CustomWidget(tk.Frame):
                                     1: ("檔案歸檔路徑不存在", "red")}
 
         self.remove_callback = remove_callback
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
 
         self.root = ttk.LabelFrame(self, text=self.n)
-        self.root.grid(padx=1, pady=1, ipadx=1, ipady=1, sticky="EW")
+        self.root.grid(row=0, column=0, padx=1, pady=1, ipadx=1, ipady=1, sticky="EW")
+        self.root.grid_rowconfigure(0, weight=1)
+        self.root.grid_columnconfigure(0, weight=1)
+        self.root.grid_columnconfigure(1, weight=1)
 
         self.filenameLabel = ttk.Label(self.root, text="檔案名稱")
-        self.filenameLabel.grid(ipadx=10, padx=10, pady=0, row=0, column=0)
+        self.filenameLabel.grid(ipadx=1, padx=5, pady=0, row=0, column=0, sticky="W")
 
-        self.filenameEntry = ttk.Entry(self.root, textvariable=self.filename, width=10)
-        self.filenameEntry.grid(ipadx=10, padx=1, pady=0, row=1, column=0)
+        self.filenameEntry = ttk.Entry(self.root, textvariable=self.filename)
+        self.filenameEntry.grid(ipadx=1, padx=5, pady=0, row=1, column=0, sticky="EW")
 
-        self.pathLabel = ttk.Label(self.root, text=self.path_label_text_map[0][0], foreground=self.path_label_text_map[0][1])
-        self.pathLabel.grid(ipadx=10, padx=10, pady=0, row=0, column=1)
+        self.pathLabel = ttk.Label(self.root,
+                                   text=self.path_label_text_map[0][0],
+                                   foreground=self.path_label_text_map[0][1])
+        self.pathLabel.grid(ipadx=1, padx=5, pady=0, row=0, column=1, sticky="W")
 
-        self.pathEntry = ttk.Entry(self.root, textvariable=self.path, width=35)
-        self.pathEntry.grid(ipadx=10, padx=1, pady=0, row=1, column=1)
+        self.pathEntry = ttk.Entry(self.root, textvariable=self.path)
+        self.pathEntry.grid(ipadx=1, padx=5, pady=0, row=1, column=1, sticky="EW")
 
         self.pathButton = ttk.Button(self.root, text="選擇資料夾", command=self.select_path, width=10)
         self.pathButton.grid(ipadx=10, padx=1, pady=0, row=1, column=2)
@@ -94,20 +101,77 @@ class CustomWidget(tk.Frame):
         if messagebox.askokcancel("刪除", "是否真的要移除?"):
             self.remove_callback(self)
 
+class scrollableContainer(tk.Frame):
+    """A scrollable container that can contain a number of messages"""
+
+    def __init__(self, master, **kwargs):
+        tk.Frame.__init__(self, master, **kwargs) #holds canvas & scrollbars
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        self.canv = tk.Canvas(self, bd=0, highlightthickness=0)
+        self.vScroll = ttk.Scrollbar(self, orient='vertical', command=self.canv.yview)
+        self.vScroll.grid(row=0, column=1, sticky='ns')
+        self.canv.grid(row=0, column=0, sticky='nsew')
+        self.canv.configure(yscrollcommand=self.vScroll.set)
+
+        self.frm = tk.Frame(self.canv, bd=2) #holds widget
+        self.frm.grid_columnconfigure(0, weight=1)
+
+        self.canv.create_window(0, 0, window=self.frm, anchor='nw', tags='inner')
+
+        self.widgets = []
+
+        self.update_layout()
+        self.canv.bind('<Configure>', self.on_configure)
+
+    def update_layout(self):
+        self.frm.update_idletasks()
+        self.canv.configure(scrollregion=self.canv.bbox('all'))
+        self.canv.yview('moveto', '1.0')
+        self.size = self.frm.grid_size()
+
+    def on_configure(self, event):
+        w, h = event.width, event.height
+        natural = self.frm.winfo_reqwidth()
+        self.canv.itemconfigure('inner', width=w if w > natural else natural)
+        self.canv.configure(scrollregion=self.canv.bbox('all'))
+
+    def add_widget(self, data):
+        w = CustomWidget(self.frm, self.size[1]+1, self.remove, data)
+        w.grid(row=self.size[1], column=0, padx=2, pady=2, sticky='we')
+        self.widgets.append(w)
+        self.update_layout()
+
+    def get_widgets(self):
+        return self.widgets
+
+    def remove(self, widget):
+        self.widgets.remove(widget)
+        widget.grid_forget()
+        for i in range(len(self.widgets)):
+            self.widgets[i].set_label_n(str(i+1))
+            self.widgets[i].grid(row=(i+1), column=0, sticky="EW")
+        self.update_layout()
 
 class Application(ttk.Frame):
-    def __init__(self, parent):
-        super(Application, self).__init__(parent, width=800, height=400)
+    def __init__(self, parent, **kwargs):
+        super(Application, self).__init__(parent, **kwargs)
+
+        self.grid_rowconfigure(0, weight=0)
+        self.grid_rowconfigure(1, weight=0)
+        self.grid_rowconfigure(2, weight=1)
+        self.grid_columnconfigure(0, weight=1)
 
         self.data = os.getcwd() + "\data"
-        self.applicationRoot = ttk.Frame(self)
-        self.applicationRoot.grid(row=0, column=0, padx=30, pady=5)
 
         # first
-        self.first = ttk.LabelFrame(self.applicationRoot, text="A.選擇要歸類處理的資料夾")
-        self.first.grid(row=0, column=0, padx=1, pady=10, ipadx=175, ipady=1)
+        self.first = ttk.LabelFrame(self, text="選擇要歸類處理的資料夾")
+        self.first.grid(row=0, column=0, padx=10, pady=1, ipadx=0, ipady=1, sticky="NEW")
         self.first.grid_columnconfigure(0, weight=1)
         self.first.grid_rowconfigure(0, weight=1)
+        self.first.grid_rowconfigure(1, weight=1)
+        self.first.grid_rowconfigure(2, weight=1)
 
         self.label_text_map = {0: ("", "black"),
                                1: ("歸類處理的資料夾不存在", "red"),
@@ -127,10 +191,12 @@ class Application(ttk.Frame):
         self.startButton.grid(row=2, column=0, sticky="EWNS", columnspan=2)
 
         # second
-        self.second = ttk.LabelFrame(self.applicationRoot, text="B.歸類檔案項目設定")
-        self.second.grid(row=1, column=0, padx=1, pady=5, ipadx=230, ipady=1)
+        self.second = ttk.LabelFrame(self, text="歸類檔案項目設定")
+        self.second.grid(row=1, column=0, padx=10, pady=5, ipadx=1, ipady=1, sticky="EW")
         self.second.grid_columnconfigure(0, weight=1)
         self.second.grid_rowconfigure(0, weight=1)
+        self.second.grid_rowconfigure(1, weight=1)
+        self.second.grid_rowconfigure(2, weight=1)
 
         self.addSettingButton = ttk.Button(self.second, text=" 儲存設定", command=self.save)
         self.addSettingButton.grid(row=0, column=0, sticky="EW")
@@ -142,25 +208,13 @@ class Application(ttk.Frame):
         self.addSettingButton.grid(row=2, column=0, sticky="EW")
 
         # third
-        self.third = ttk.LabelFrame(self.applicationRoot, text="C.歸類檔案項目")
-        self.third.grid(row=2, column=0, padx=1, pady=5, ipadx=1, ipady=1, sticky="EW")
+        self.third = ttk.LabelFrame(self, text="歸類檔案項目")
+        self.third.grid(row=2, column=0, padx=10, pady=5, ipadx=1, ipady=1, sticky="NSEW")
         self.third.grid_columnconfigure(0, weight=1)
         self.third.grid_rowconfigure(0, weight=1)
 
-        self.canvas = tk.Canvas(self.third, borderwidth=0)
-        self.canvas.grid(row=0, column=0, sticky="NSWE")
-
-        self.frame = tk.Frame(self.canvas)
-
-        self.vsb = ttk.Scrollbar(self.third, orient="vertical", command=self.canvas.yview)
-        self.vsb.grid(row=0, column=1, sticky="NS")
-        self.canvas.configure(yscrollcommand=self.vsb.set)
-
-        self.canvas.create_window((4, 4), window=self.frame, anchor="nw", tags="self.frame")
-
-        self.frame.bind("<Configure>", self.on_frame_configure)
-
-        self.customWidgetList = []
+        self.sc = scrollableContainer(self.third, bd=2)
+        self.sc.grid(row=0, column=0, sticky="NSEW")
 
     def select_working_path(self):
         path_ = askdirectory()
@@ -174,10 +228,11 @@ class Application(ttk.Frame):
         file_path = os.path.join(self.workingDirectoryPath.get(), file)
         if os.path.isfile(file_path):
 
-            if len(self.customWidgetList) == 0:
+            widgets = self.sc.get_widgets()
+            if len(widgets) == 0:
                 self.set_label(3)
 
-            for widget in self.customWidgetList:
+            for widget in widgets:
                 if widget.get_filename() in file:
                     to_folder = widget.get_path()
                     if os.path.isdir(to_folder):
@@ -208,11 +263,11 @@ class Application(ttk.Frame):
     def save(self):
         with open(self.data, "w") as f:
             f.write(json.dumps([{"path": self.workingDirectoryPath.get()}]) + "\n")
-            for i in range(len(self.customWidgetList)):
-                f.write(self.customWidgetList[i].get_json_string() + "\n")
+            widgets = self.sc.get_widgets()
+            for widget in widgets:
+                f.write(widget.get_json_string() + "\n")
 
     def load(self):
-        self.customWidgetList.clear()
         with open(self.data, "r") as f:
             line = f.readline()
             if line is not "":
@@ -220,48 +275,31 @@ class Application(ttk.Frame):
                 self.workingDirectoryPathEntry.delete(0, tk.END)
                 self.workingDirectoryPathEntry.insert(0, data[0]["path"])
             for line in f:
-                widget = CustomWidget(self.frame, "", self.remove, line)
-                self.customWidgetList.append(widget)
-                i = int(widget.get_label_n())
-                widget.grid(row=i, column=0, sticky="EW")
-                self.grid_rowconfigure(i - 1, weight=1)
+                self.add(line)
 
-    def add(self):
-        i = len(self.customWidgetList) + 1
-        widget = CustomWidget(self.frame, str(i), self.remove)
-        self.customWidgetList.append(widget)
-        widget.grid(row=i, column=0, sticky="EW")
-        self.grid_rowconfigure(i - 1, weight=1)
-
-    def remove(self, custom_widget):
-        self.customWidgetList.remove(custom_widget)
-        custom_widget.grid_forget()
-        for i in range(len(self.customWidgetList)):
-            self.customWidgetList[i].set_label_n(str(i+1))
-            self.customWidgetList[i].grid(row=(i+1), column=0, sticky="EW")
+    def add(self, data=""):
+        self.sc.add_widget(data)
 
     def moveto(self, filename, from_folder, to_folder):
         from_file = os.path.join(from_folder, filename)
         to_file = os.path.join(to_folder, filename)
-        # to move only files, not folders
         if not to_file == from_file:
-            #print('moved: ' + str(to_file))
             if os.path.isfile(from_file):
                 if not os.path.exists(to_folder):
                     os.makedirs(to_folder)
                 os.rename(from_file, to_file)
 
-    def on_frame_configure(self, event):
-        '''Reset the scroll region to encompass the inner frame'''
-        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
-
 if __name__ == "__main__":
     root = tk.Tk()
-    root.resizable(False, False)
-    root.title("檔案自動分類")
+    #root.resizable(False, False)
+    root.title("檔案歸類")
+    root.iconbitmap("office.ico")
+    root.geometry('800x600')
+    root.grid_columnconfigure(0, weight=1)
+    root.grid_rowconfigure(0, weight=1)
 
     app = Application(root)
-    app.grid(row=0, column=0)
+    app.grid(row=0, column=0, pady=10, sticky="NSEW")
 
     #def on_close():
     #    app.save()
